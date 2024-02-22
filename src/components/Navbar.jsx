dc1a2c271fab873343a1593ccdb69d1789735d7f
sanalkumars <sanalskumar126@gmail.com>
2024-02-22 21:39:32 +0530

    added framer animation for menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link'
 import React, { useState } from 'react'
 import NavLink from './navLink';
+import {motion} from 'framer-motion';
 
 
 const links =[
@@ -15,10 +16,45 @@ const links =[
 
 const Navbar = () => {
   const [open,setOpen] = useState(false)
+
+  const topVariants = {
+    closed: {
+      rotate:  0,
+    },
+    opened: {
+      rotate:  45,
+      backgroundColor: "rgb(255,255,255)", // Removed leading space
+    },
+  };
+  
+  const centerVariants = {
+    closed: {
+      opacity:  1,
+    },
+    opened: {
+      opacity:  0,
+    },
+  };
+  
+  const bottomVariants = {
+    closed: {
+      rotate:  0,
+    },
+    opened: {
+      opacity:  1,
+      rotate: -45, 
+      backgroundColor: "rgb(255,255,255)", 
+    },
+  };
+  
+
+
+
+
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
       {/* links */}
-        <div className='hidden md:flex gap-4 w-1/3'>
+        <div className='hidden md:flex text-black gap-4 w-1/3'>
           {links.map(link =>(
             <NavLink link={link} key={link.title} />
           ))}
@@ -54,9 +90,21 @@ const Navbar = () => {
         {/* menu button */}
         <button className='w-10 h-8 flex flex-col justify-between z-50 relative'
           onClick={() =>setOpen(!open)}>
-          <div className='w-10 h-1 bg-white rounded'></div>
-          <div className='w-10 h-1 bg-white rounded'></div>
-          <div className='w-10 h-1 bg-white rounded'></div>
+          <motion.div
+          variants={topVariants} 
+          animate={open ? "opened" : "closed"}
+           className='w-10 h-1 bg-black rounded origin-left'
+           ></motion.div>
+          <motion.div 
+          variants={centerVariants} 
+          animate={open ? "opened" : "closed"} 
+          className='w-10 h-1 bg-black rounded'
+          ></motion.div>
+          <motion.div 
+          variants={bottomVariants} 
+          animate={open ? "opened" : "closed"}
+          className='w-10 h-1 bg-black rounded origin-left'
+          ></motion.div>
         </button>
         {open && (
               <div className='absolute top-3 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -8,12 +8,13 @@ import { usePathname } from "next/navigation";
  const NavLink = ({link}) => {
 
     const pathName = usePathname()
-
+  
   return (
     <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white "} `}
          href={link.url}>
          {link.title}
     </Link>
+   
   )
 }
 
